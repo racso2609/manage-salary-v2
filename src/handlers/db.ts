468ec34f367e9fbc645049ecdeaf99148dc30769
racso <oscaremiliolugo@gmail.com>
2025-02-22 12:38:40 -0400

    db handler

---
+++ src/handlers/db.ts
@@ -0,0 +1,121 @@
+import {
+  Model,
+  FilterQuery,
+  UpdateWriteOpResult,
+  UpdateQuery,
+  PipelineStage,
+  isValidObjectId,
+  Types,
+} from "mongoose";
+import "@/models/index";
+import { OverrideDbParams, DbRepository as Repository } from "@/types/db";
+
+export class DbRepository<T> implements Repository<T> {
+  constructor(protected model: Model<T>) {}
+
+  protected _manageQuery(
+    query: FilterQuery<T>,
+    overrideDbParams: OverrideDbParams = {},
+  ) {
+    const parsedQuery = Object.keys(query).reduce(
+      (acc, key) => {
+        const value = query[key];
+        const isId = isValidObjectId(value);
+        acc[key] = isId ? new Types.ObjectId(value) : value;
+        return acc;
+      },
+      {} as Record<string, unknown>,
+    );
+    const aggregateQuery: PipelineStage[] = [{ $match: parsedQuery }];
+
+    if (overrideDbParams?.sort) {
+      const parsedSort = Object.keys(overrideDbParams.sort).reduce(
+        (acc, key) => {
+          if (overrideDbParams.sort)
+            acc[key] = overrideDbParams.sort[key] === "asc" ? 1 : -1;
+          return acc;
+        },
+        {} as Record<string, 1 | -1>,
+      );
+
+      aggregateQuery.push({ $sort: parsedSort });
+    }
+    if (overrideDbParams?.populates?.length) {
+      overrideDbParams.populates.forEach(({ path: populate, unique }) => {
+        aggregateQuery.push({
+          $lookup: {
+            from: populate + "s",
+            localField: populate,
+            foreignField: "_id",
+            as: populate,
+          },
+        });
+        if (unique)
+          aggregateQuery.push({
+            $unwind: {
+              path: `$${populate}`,
+              preserveNullAndEmptyArrays: true,
+            },
+          });
+      });
+    }
+
+    if (overrideDbParams?.group)
+      aggregateQuery.push({ $group: overrideDbParams.group });
+
+    // isValidObjectId()
+
+    console.log("=== aggregateQuery", aggregateQuery);
+
+    return aggregateQuery;
+  }
+
+  find(query: FilterQuery<T>, overrideDbParams: OverrideDbParams = {}) {
+    const aggregateQuery = this._manageQuery(query, overrideDbParams);
+    return this.aggregate(aggregateQuery);
+  }
+
+  async findOne(
+    query: FilterQuery<T>,
+    overrideDbParams: OverrideDbParams = {},
+  ) {
+    const aggregateQuery = this._manageQuery(query, overrideDbParams);
+    const result = await this.aggregate(aggregateQuery);
+    return result.at(0);
+  }
+
+  async create(data: T) {
+    return await this.model.create(data);
+  }
+
+  async updateOne(query: FilterQuery<T>, data: UpdateQuery<T>) {
+    return await this.model.findOneAndUpdate(query, data, { new: true });
+  }
+
+  async update(
+    query: FilterQuery<T>,
+    data: UpdateQuery<T>,
+  ): Promise<UpdateWriteOpResult> {
+    return await this.model.updateMany(query, data, { new: true });
+  }
+
+  async delete(query: FilterQuery<T>): Promise<boolean> {
+    const deleteResult = await this.model.deleteOne(query);
+    return deleteResult.deletedCount > 0;
+  }
+  async deleteMany(query: FilterQuery<T>): Promise<boolean> {
+    const deleteResult = await this.model.deleteMany(query);
+    return deleteResult.deletedCount > 0;
+  }
+
+  async updateOrCreate(query: FilterQuery<T>, data: UpdateQuery<T>) {
+    return await this.model.findOneAndUpdate(query, data, {
+      upsert: true,
+      new: true,
+    });
+  }
+
+  aggregate(pipeline: PipelineStage[]) {
+    return this.model.aggregate(pipeline);
+  }
+}
---
+++ src/types/db.ts
@@ -0,0 +1,38 @@
+import {
+  FilterQuery,
+  Document,
+  UpdateQuery,
+  UpdateWriteOpResult,
+  SortOrder,
+} from "mongoose";
+
+export type OverrideDbParams = Partial<{
+  sort: Record<string, SortOrder>;
+  group: Record<string, unknown> & { _id: string };
+  populates: { path: string; unique?: boolean }[];
+}>;
+
+export type DbRepository<T> = {
+  find(
+    query: FilterQuery<T>,
+    overrideParams?: OverrideDbParams,
+  ): Promise<Document<unknown, unknown, T>[]>;
+  findOne(
+    query: FilterQuery<T>,
+    overrideParams?: OverrideDbParams,
+  ): Promise<Document<unknown, unknown, T> | null>;
+  create(data: T): Promise<T>;
+  updateOne(
+    query: FilterQuery<T>,
+    data: UpdateQuery<T>,
+  ): Promise<Document<unknown, unknown, T> | null>;
+  update(
+    query: FilterQuery<T>,
+    data: UpdateQuery<T>,
+  ): Promise<UpdateWriteOpResult>;
+  delete(query: FilterQuery<T>): Promise<boolean>;
+  updateOrCreate(
+    query: FilterQuery<T>,
+    data: UpdateQuery<T>,
+  ): Promise<Document<unknown, unknown, T> | null>;
+};
