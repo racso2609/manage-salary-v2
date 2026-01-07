06d005ed470099a029ae26798184a86891ed4e20
racso2609 <oscaremiliolugo@gmail.com>
2026-01-07 14:46:57 -0400

    fix validation

--- dist/src/middlewares/combinedAuth.js
+++ dist/src/middlewares/combinedAuth.js
@@ -6,4 +6,40 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.combinedAuth = void 0;
 const apiKeyAuth_1 = require("./apiKeyAuth");
 const passport_1 = __importDefault(require("passport"));
-exports.combinedAuth = [passport_1.default.authenticate("bearer", { session: false }), apiKeyAuth_1.apiKeyAuth];
+const combinedAuth = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    const apiKeyHeader = req.headers['x-api-key'];
+    if (authHeader) {
+        // Use bearer validation
+        passport_1.default.authenticate("bearer", { session: false }, (err, user, info) => {
+            if (err) {
+                return next(err);
+            }
+            if (user) {
+                req.user = user;
+                return next();
+            }
+            else {
+                res.status(401).json({ error: "Invalid bearer token" });
+                return;
+            }
+        })(req, res, next);
+    }
+    else if (apiKeyHeader) {
+        // Use apiKeyAuth
+        (0, apiKeyAuth_1.apiKeyAuth)(req, res, () => {
+            if (req.user) {
+                return next();
+            }
+            else {
+                res.status(401).json({ error: "Invalid API key" });
+                return;
+            }
+        });
+    }
+    else {
+        res.status(401).json({ error: "No authentication provided" });
+        return;
+    }
+};
+exports.combinedAuth = combinedAuth;
--- src/middlewares/combinedAuth.ts
+++ src/middlewares/combinedAuth.ts
@@ -1,4 +1,37 @@
-import { apiKeyAuth } from "./apiKeyAuth";
-import passport from "passport";
+ import { apiKeyAuth } from "./apiKeyAuth";
+ import passport from "passport";
+ import { Request, Response, NextFunction } from "express";
 
-export const combinedAuth = [passport.authenticate("bearer", { session: false }), apiKeyAuth];
+ export const combinedAuth = (req: Request, res: Response, next: NextFunction) => {
+   const authHeader = req.headers.authorization;
+   const apiKeyHeader = req.headers['x-api-key'] as string;
+
+   if (authHeader) {
+     // Use bearer validation
+     passport.authenticate("bearer", { session: false }, (err, user, info) => {
+       if (err) {
+         return next(err);
+       }
+       if (user) {
+         req.user = user;
+         return next();
+       } else {
+         res.status(401).json({ error: "Invalid bearer token" });
+         return;
+       }
+     })(req, res, next);
+   } else if (apiKeyHeader) {
+     // Use apiKeyAuth
+     apiKeyAuth(req, res, () => {
+       if ((req as any).user) {
+         return next();
+       } else {
+         res.status(401).json({ error: "Invalid API key" });
+         return;
+       }
+     });
+   } else {
+     res.status(401).json({ error: "No authentication provided" });
+     return;
+   }
+ };
